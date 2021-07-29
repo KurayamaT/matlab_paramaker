f234e7ff004b0648f9bb71bb13dae26de61916d6
KurayamaT <t.kurayama@nifty.com>
2021-07-29 21:28:38 +0900

    Taichi Kurayama  21:25 スクリプト、大体ok、ファイルを二つに分ける必要あり（パラメータ掃き出し部とfixedpt計算部分） 計算範囲、もう少し吟味が必要だがほぼほぼ決まり です。続き明日にします。 （編集済み）

--- paramaker.m
+++ paramaker.m
@@ -1,19 +1,25 @@
 % make initial parameters [q1(sheta), u1(sheta-dot), g(gamma)] with the
 % range of defined value.
 
-function [q1, u1, gam] = paramaker(q_start, q_end, q_points, u_start, u_end, u_points, gam_start, gam_end, gam_points);
+function paramaker(q_start, q_end, q_points, u_start, u_end, u_points, gam_start, gam_end, gam_points);
+
+format long
 
 delete q1.mat;
 delete u1.mat;
 delete gam.mat;
 
-q1 = (q_start:(q_end-q_start)/q_points:q_end)';
-u1 = (u_start:(u_end-u_start)/u_points:u_end)';
-gam = (gam_start:(gam_end-gam_start)/gam_points:gam_end)';
+q1  = (q_start:(q_end-q_start)/q_points:q_end);
+u1  = (u_start:(u_end-u_start)/u_points:u_end);
+gam = (gam_start:(gam_end-gam_start)/gam_points:gam_end);
 
 save q1.mat q1;
 save u1.mat u1;
 save gam.mat gam;
+
+run(runner);
+
+% paramaker(0.017453293,0.13962634,10,0.017453293,0.13962634,10,5.81776E-05,0.000523599,10)
 
 
 
