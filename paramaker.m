2772e09ce38e1e9da78d5b2d82d7bf2718498e5b
KurayamaT <t.kurayama@nifty.com>
2021-08-11 18:06:56 +0900

    20210811c

--- paramaker.m
+++ paramaker.m
@@ -1,4 +1,5 @@
-% make initial parameters [q1(sheta), u1(sheta-dot), g(gamma)] with the range of defined value.
+% make initial parameters [q1(theta), u1(theta-dot), g(gamma)] with the range of defined value.
+% θの開始と終了値、point数、θ'の開始と終了値、point数、γの開始と終了値、point数を入力。
 
 function [q,u,gamma] = paramaker(q_start, q_end, q_points, u_start, u_end, u_points, gam_start, gam_end, gam_points);
 %%%実行するときはここで定義しているfunction名はあまり関係なかったりする。ファイル名そのものに依存。
@@ -13,13 +14,10 @@ q  = q_start:(q_end-q_start)/q_points:q_end;
 u  = u_start:(u_end-u_start)/u_points:u_end;
 gamma = gam_start:(gam_end-gam_start)/gam_points:gam_end;
 
-
-
 passivewalker_k(q,u,gamma)
 % save q1.mat q;
 % save u1.mat u;
 % save gam.mat gam;
-
 
 %%% ↓お試し用：入力数値コマンド
 % paramaker(0.017453293,0.13962634,3,0.017453293,0.13962634,3,5.81776E-05,0.000523599,3)
